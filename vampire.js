a1605fe6a41bc8ed4473777c0083ed3203a56226
Gabriel Varadi <gabriel.varadi.hallope.lecoq@gmail.com>
2019-05-29 17:40:16 +0000

    Exercise

--- vampire.js
+++ vampire.js
@@ -10,22 +10,35 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
+    this.offspring.push(vampire)
+    vampire.creator = this
 
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let sum = 0
+    let baby = this
+    while (baby.creator) {
+      baby = baby.creator
+      sum++
+    }
+    return sum
 
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true
+    } else {
+      return false
+    }
   }
 
   /** Stretch **/
@@ -37,8 +50,25 @@ class Vampire {
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
 
+    let vampire1 = this
+
+    if (!vampire1.creator) {
+      return vampire1
+    }
+    if (!vampire.creator) {
+      return vampire
+    }
+
+    while (vampire1.creator && vampire.creator) {
+      if (vampire1.creator === vampire.creator) {
+        return vampire1.creator
+      } else if (vampire.isMoreSeniorThan > vampire1.isMoreSeniorThan){
+        vampire = vampire.creator
+      } else if (vampire.isMoreSeniorThan < vampire1.isMoreSeniorThan){
+        vampire1 = vampire1.creator
+      }
+
+    }
   }
 }
-
 module.exports = Vampire;
-
